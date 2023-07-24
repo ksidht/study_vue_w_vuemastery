f4185157af3b689649e5e20eab9dbecd79d44455
root <root@SiddharthK.ClaimGeniusInc.com>
2023-07-24 18:55:59 +0530

    Vue setup

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import EventCard from './components/EventCard.vue'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <!-- <EventCard msg="You did it!" /> -->
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  align-items: center;
+}
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+h2 {
+  font-size: 20px;
+}
+
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+import { ref } from "vue";
+
+const total = ref(0)
+
+
+function updateTotal() {
+  console.log(total.value)
+  total.value += 1
+}
+
+</script>
+
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    
+    <br/>
+    <div>
+    <button @click="updateTotal">
+      Plus
+    </button>
+  </div>
+    <div>
+      {{ total }}
+    </div>
+
+  </div>
+</template>
+
+
+<style>
+
+</style>
